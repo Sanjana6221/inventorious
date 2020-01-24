72f3c2d307b44f21dc0db335d2eb92553d728638
sanjana <sanjnarathore1995@gmail.com>
2020-01-24 12:15:30 +0530

    delete element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
-import React, {ComponentElement} from "react";
+import React, {Component} from 'react'
+import Table from './Table'
 
-export default function App() {
-  return (
-    
-  );
+export default class App extends Component {
+  state = {
+    characters: [
+      {
+        name: "Sanjana",
+        job: "Software Development",
+      },
+      {
+        name: "ABC",
+        job: "Tester",
+      },
+      {
+        name: "Dee",
+        job: "Aspiring actress",
+      },
+      {
+        name: "Dennis",
+        job: "Bartender",
+      }
+    ],
+  }
+
+  removeCharacter = (element) => {
+    const {characters} = this.state
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== element
+      })
+    })
+  }
+
+  render() {
+    const tableData = this.state
+
+    return (
+      <div className="container">
+        <Table tableData={tableData} removeCharacter={this.removeCharacter}/>
+      </div>
+    )
+  }
 }
